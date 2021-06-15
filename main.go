2734b8edd7f5b7a2eac788e3e92164daaa368380
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-06-15 21:41:16 +0900

    add nats sample code
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+const (
+	subscription = "ORDERS.scratch"
+	consumerName = "nats-consumer"
+)
+
+func run(ctx context.Context) error {
+	// Connect to a server
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		return fmt.Errorf("failed to connect: %w", err)
+	}
+
+	js, err := nc.JetStream(nats.PublishAsyncMaxPending(256))
+	if err != nil {
+		return fmt.Errorf("failed to apply: %w", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		js.PublishAsync(subscription, []byte("hello"))
+	}
+	select {
+	case <-js.PublishAsyncComplete():
+	case <-time.After(5 * time.Second):
+		return errors.New("publish did not resolve in time")
+	}
+	// Simple Pull Consumer
+	sub, err := js.PullSubscribe(subscription, consumerName)
+	if err != nil {
+		return fmt.Errorf("failed to create subscription: %w", err)
+	}
+	msgs, err := sub.Fetch(10)
+	if err != nil {
+		return fmt.Errorf("failed to fetch messages: %w", err)
+	}
+
+	for _, msg := range msgs {
+		fmt.Println(string(msg.Data))
+		if err := msg.Ack(); err != nil {
+			log.Printf("failed to ack: %s\n", err.Error())
+		}
+	}
+
+	sub.Unsubscribe()
+
+	return nil
+}
 
 func main() {
-	fmt.Println("hello")
+	if err := run(context.Background()); err != nil {
+		log.Fatalf("error occurs: %s", err.Error())
+	}
 }
